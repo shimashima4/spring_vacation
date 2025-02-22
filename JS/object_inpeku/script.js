6dfd02593da8c25d158839b2d53ccebad3849927
shimashima4 <aoshima.4ma@gmail.com>
2025-02-22 10:53:49 +0900

    スコープがどうなっているか確かめる

--- JS/object_inpeku/script.js
+++ JS/object_inpeku/script.js
@@ -1,10 +1,26 @@
+let _level = "99";
 // オブジェクト
 const metaX = () => {
-  let _level = "50";
+  let _level = "60";
   return {
     level() {
       return _level;
     },
   };
 };
+
+// アロー関数じゃない方
+const metaXalpha = function () {
+  let _level = "1";
+  return {
+    level() {
+      return _level;
+    },
+  };
+};
+
 console.log(`レベル：${metaX().level()}`);
+console.log(`レベル：${metaXalpha().level()}`);
+console.log(`最大レベル：${_level}`);
+// 同じ名前の変数を定義しているが、
+// スコープが関数内だけなので問題ない
